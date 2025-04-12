d9e9551b360717f8a9ee88c123a76829620ab28d
btag <btag2023@gmail.com>
2025-04-12 12:11:41 +0330

    add *ngIf and *ngFor directive in greeting component

--- src/app/greeting/greeting.component.ts
+++ src/app/greeting/greeting.component.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-greeting',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './greeting.component.html',
   styleUrl: './greeting.component.css'
 })
 export class GreetingComponent {
 name='angular';
 count=0;
+showMessage=true;
+users= ['ali','Nilufar','btag','mohammad','Shahla'];
 
 sayHello(){
   alert('Hello from ${this.name}!');
